4f58ea8119832964ba12c669d39bb8c3cb5d043c
samarheydari <samarheydari76@gmail.com>
2026-08-10 16:09:44 +0200

    first cleaning

--- paper-camera-ready/paper/paper/12-supp/code/scripts/run_plot_concept_backgroundiness.sh
+++
@@ -1,9 +0,0 @@
-python3 -m experiments.plot_concept_backgroundiness --model_name unet --dataset_name cityscapes --labels "CRP Relevance,LRP,Activation"
-python3 -m experiments.plot_concept_backgroundiness --model_name deeplabv3plus --dataset_name voc2012 --labels "CRP Relevance,LRP,Activation"
-python3 -m experiments.plot_concept_backgroundiness --model_name yolov6 --dataset_name coco2017 --labels "CRP Relevance,LRP,Activation"
-python3 -m experiments.plot_concept_backgroundiness --model_name yolov5 --dataset_name coco2017 --labels "CRP Relevance,LRP,Activation"
-
-python3 -m experiments.plot_concept_backgroundiness --model_name unet --dataset_name cityscapes --labels "CRP Relevance,Guided GradCAM,GradCAM,SSGradCAM"
-python3 -m experiments.plot_concept_backgroundiness --model_name deeplabv3plus --dataset_name voc2012 --labels "CRP Relevance,Guided GradCAM,GradCAM,SSGradCAM"
-python3 -m experiments.plot_concept_backgroundiness --model_name yolov6 --dataset_name coco2017 --labels "CRP Relevance,Guided GradCAM,GradCAM,SSGradCAM"
-python3 -m experiments.plot_concept_backgroundiness --model_name yolov5 --dataset_name coco2017 --labels "CRP Relevance,Guided GradCAM,GradCAM,SSGradCAM"
--- paper_sanity_check/LCRP/models/SSD/ssd/modeling/registry.py
+++
@@ -1,5 +0,0 @@
-from models.SSD.ssd.utils.registry import Registry
-
-BACKBONES = Registry()
-BOX_HEADS = Registry()
-BOX_PREDICTORS = Registry()
